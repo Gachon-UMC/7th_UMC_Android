a36fae8a0952af4964eb50c25839be54cbc0192f
hdg0116 <hdg0116@gmail.com>
2024-11-30 00:14:51 +0900

    9주차 과제 임시 제출

---
+++ joy/week9_FLO/app/src/main/java/com/example/flo/AuthRetrofitInterface.kt
@@ -0,0 +1,13 @@
+package com.example.flo
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthRetrofitInterface {
+    @POST("/join")
+    fun signUp(@Body user : User) : Call<AuthResponse>
+
+    @POST("/login")
+    fun logIn(@Body user : User) : Call<AuthResponse>
+}
---
+++ joy/week9_FLO/app/src/main/java/com/example/flo/AuthService.kt
@@ -0,0 +1,79 @@
+package com.example.flo
+
+import android.util.Log
+import android.widget.Toast
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class AuthService {
+    private lateinit var signUpView: SignUpView
+    private lateinit var logInView: LogInView
+
+    fun setSignUpView(signUpView: SignUpView) {
+        this.signUpView = signUpView
+    }
+
+    fun setLogInView(logInView: LogInView) {
+        this.logInView = logInView
+    }
+
+    fun signUp(user : User) {
+        val signUpService = getRetrofit().create(AuthRetrofitInterface::class.java)
+
+        signUpService.signUp(user).enqueue(object : Callback<AuthResponse> {
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                Log.d("SIGNUP/RESPONSE", response.toString())
+                Log.d("SIGNUP/RESPONSE_CODE", response.code().toString())
+                Log.d("SIGNUP/RESPONSE_BODY", response.body()?.toString() ?: "Response body is null")
+
+                val resp: AuthResponse = response.body()!!
+
+                if (resp == null || !resp.isSuccess) {
+                    signUpView.onSignUpFailure()
+                    return
+                }
+
+                signUpView.onSignUpSuccess()
+            }
+
+
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                Log.d("SIGNUP/FAILURE", t.message.toString())
+                signUpView.onSignUpFailure()
+            }
+        })
+
+        Log.d("SignUpActivity", "All Finished")
+    }
+
+    fun logIn(user : User) {
+        val authService = getRetrofit().create(AuthRetrofitInterface::class.java)
+
+        authService.logIn(user).enqueue(object : Callback<AuthResponse> {
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                Log.d("LOGIN/SUCCESS", response.toString())
+                Log.d("LOGIN/RESPONSE_CODE", response.code().toString())
+                Log.d("LOGIN/RESPONSE_BODY", response.body()?.toString() ?: "Response body is null")
+                Log.d("LOGIN/REQUEST_BODY", Gson().toJson(user))
+
+                val resp: AuthResponse = response.body()!!
+
+                if (resp == null || !resp.isSuccess) {
+                    logInView.onLogInFailure()
+                    return
+                }
+
+                logInView.onLogInSuccess(resp.isSuccess)
+            }
+
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                Log.d("LOGIN/FAILURE", t.message.toString())
+                logInView.onLogInFailure()
+            }
+        })
+
+        Log.d("LoginActivity", "All Finished")
+    }
+}
